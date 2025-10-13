f691511081265d414c03780d3706a27883165734
RagavMuthukumar <sathyaragavan.m@gmail.com>
2025-10-13 23:51:18 +0530

    final push

--- backend/app.py
+++ backend/app.py
@@ -4,8 +4,9 @@ import MySQLdb.cursors
 import os
 
 app = Flask(__name__)
-app.secret_key = 'secret123'
+app.secret_key = 'your_secret_key'  # Change this to a strong secret key
 
+# MySQL configuration from environment variables or defaults
 app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST', 'localhost')
 app.config['MYSQL_USER'] = os.getenv('MYSQL_USER', 'root')
 app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD', 'root')
@@ -14,6 +15,26 @@ app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
 mysql = MySQL(app)
 
+# Route for login page (index.html)
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    msg = ''
+    if request.method == 'POST' and 'email' in request.form and 'password' in request.form:
+        email = request.form['email']
+        password = request.form['password']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM accounts WHERE email = %s AND password = %s', (email, password))
+        account = cursor.fetchone()
+        if account:
+            session['loggedin'] = True
+            session['id'] = account['id']
+            session['email'] = account['email']
+            return redirect(url_for('dashboard'))
+        else:
+            msg = 'Incorrect email or password!'
+    return render_template('index.html', msg=msg)
+
+# Route for registration page (register.html)
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     msg = ''
@@ -28,29 +49,19 @@ def register():
         elif not email or not password:
             msg = 'Please fill out the form!'
         else:
-            cursor.execute('INSERT INTO accounts(email, password) VALUES (%s, %s)', (email, password))
+            cursor.execute('INSERT INTO accounts (email, password) VALUES (%s, %s)', (email, password))
             mysql.connection.commit()
             msg = 'You have successfully registered!'
     return render_template('register.html', msg=msg)
 
-@app.route('/', methods=['GET', 'POST'])
-def login():
-    msg = ''
-    if request.method == 'POST' and 'email' in request.form and 'password' in request.form:
-        email = request.form['email']
-        password = request.form['password']
-        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM accounts WHERE email = %s AND password = %s', (email, password))
-        account = cursor.fetchone()
-        if account:
-            session['loggedin'] = True
-            session['id'] = account['id']
-            session['email'] = account['email']
-            return f"Welcome {account['email']}! <br><a href='/logout'>Logout</a>"
-        else:
-            msg = 'Incorrect email or password!'
-    return render_template('index.html', msg=msg)
+# Dashboard route after login
+@app.route('/dashboard')
+def dashboard():
+    if 'loggedin' in session:
+        return f"Welcome, {session['email']}!<br><a href='/logout'>Logout</a>"
+    return redirect(url_for('login'))
 
+# Logout route
 @app.route('/logout')
 def logout():
     session.clear()
